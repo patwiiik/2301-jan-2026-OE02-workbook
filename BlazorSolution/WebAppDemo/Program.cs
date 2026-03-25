9dfea98e15ffe0751d4cd6f8e62b943c474166ea
Don Welch <dwelch@nait.ca>
2026-03-24 21:52:14 -0600

    setup connection string,  setup program.cs

--- BlazorSolution/WebAppDemo/Program.cs
+++ BlazorSolution/WebAppDemo/Program.cs
@@ -1,6 +1,39 @@
+using Microsoft.EntityFrameworkCore;
 using WebAppDemo.Components;
+using WestWindSystem.BLL;
+using WestWindSystem.DAL;
 
 var builder = WebApplication.CreateBuilder(args);
+
+// add the dbcontext and service class of my class library
+//      to the collection of services that will be made
+//      available to this web app when the web app requests
+//      said service
+
+// obtain the connection to supply to out DAL context class
+// We will be registering the db connection AND our services
+//      we create within the BLL folder of your library
+
+//grab your connection string from its declared location
+// either appsettings.json
+//     or user secrets  
+
+//access is first to appsettings.json
+//then to user secrets
+//if there is a declaration in both
+// the declaration in appsettings is over ridden with the
+//          declaration in user secrets.
+var connectionstring = builder.Configuration.GetConnectionString("WWDB");
+
+//register the DbContext to the Sql Sever
+builder.Services.AddDbContext<WestWindContext>(options => options.UseSqlServer(connectionstring));
+
+//Register your service class
+//One for EACH service class in your BLL
+//project recommendation, ADD lastname to the front of your service class
+builder.Services.AddScoped<ProductServices>();
+builder.Services.AddScoped<CategoryServices>();
+builder.Services.AddScoped<SupplierServices>();
 
 // Add services to the container.
 builder.Services.AddRazorComponents()
